930622d19bd6befdeeee71b292ae2016e80fa796
Dejan Rudic Vrnaic <dejanrudicvranic@gmail.com>
2023-02-23 15:48:23 +0100

    test2

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
- const x = 0;
-
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( metadata.name, {
+  registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
