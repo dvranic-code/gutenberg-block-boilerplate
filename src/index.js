4b191d27c34cbb8ca107cca7aa2e3ae9dafa7257
Dejan Rudic Vrnaic <dejanrudicvranic@gmail.com>
2023-02-23 15:49:18 +0100

    what

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import metadata from './block.json';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-  registerBlockType( metadata.name, {
+registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
